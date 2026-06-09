4ec162cbb008cde9d03dfe3a233a72a6373a7851
Sofia Sanchez <sofia.sanchez.forero@gmail.com>
2026-06-09 14:26:48 -0500

    Configurar start script

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("SoriTech Backend funcionando 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
